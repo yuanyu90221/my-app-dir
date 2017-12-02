048f296dc98a8b622a74637949e10f3a92759213
yuanyu90221 <yuanyu90221@gmail.com>
2017-12-03 03:11:59 +0800

    update my-app-dir with store and  storeController

---
+++ my-app-dir/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController')
+// Do work here
+router.get('/', storeController.homePage);
+
+router.get('/reverse/:name', (req, res) => {
+  // res.send('it works');
+  const reverse = [...req.params.name].reverse().join('')
+  // res.json(req.params);
+  res.send(reverse);
+})
+module.exports = router;
