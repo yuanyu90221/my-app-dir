36ce16802d6599a16e0fe7cc6272c0b48460270b
yuanyu90221 <yuanyu90221@gmail.com>
2017-12-04 03:23:22 +0800

    add editStore.pug and mixins component _storeForm.pug

--- my-app-dir/routes/index.js
+++ my-app-dir/routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const storeController = require('../controllers/storeController')
 // Do work here
 router.get('/', storeController.homePage);
-
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 router.get('/reverse/:name', (req, res) => {
   // res.send('it works');
   const reverse = [...req.params.name].reverse().join('')
