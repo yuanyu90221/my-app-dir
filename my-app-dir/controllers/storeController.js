927c03ae82b86a79ecd29e4d7a2b1a90256b048f
yuanyu90221 <yuanyu90221@gmail.com>
2017-12-10 03:10:20 +0800

    add update store flow and view

--- my-app-dir/controllers/storeController.js
+++ my-app-dir/controllers/storeController.js
@@ -1,9 +1,11 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'Wes';
-    next();
-}
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+// exports.myMiddleware = (req, res, next) => {
+//     req.name = 'Wes';
+//     next();
+// }
 exports.homePage = (req, res) => {
-    console.log(req.name);
     res.render('index');
 }
 
@@ -13,6 +15,36 @@ exports.addStore = (req, res) => {
     });
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    await store.save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review? `);
+    res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+    // 1 Query the database for a list of all stores
+    const stores = await Store.find();
+    console.log(stores);
+    res.render('stores', {title: 'Stores', stores});
+}
+
+exports.editStore = async (req, res) => {
+    // 1. Find the stores given the ID
+    const store = await Store.findOne({_id: req.params.id});
+    // 2. confirm they are the owner of the store
+    // TODO
+    // 3. Render out the edit form so the user can update their store
+    res.render('editStore', {title: `Edit ${store.name}`, store});
+}
+
+exports.updateStore = async (req, res) => {
+    // Find and update store
+    const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
+        new: true, // return the new store instead of the old one
+        runValidators: true
+    }).exec(); 
+    req.flash('success', `Successfully updated <strong>${store.name}</strong> <a href="/stores/${store.slug}">View Store →</a>`);
+    // Redirect them the store and tell them it works
+    res.redirect(`/stores/${store._id}/edit`);
 }
--- my-app-dir/routes/index.js
+++ my-app-dir/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const storeController = require('../controllers/storeController')
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
 // Do work here
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add',catchErrors(storeController.createStore));
+router.post('/add/:id',catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 router.get('/reverse/:name', (req, res) => {
   // res.send('it works');
   const reverse = [...req.params.name].reverse().join('')
