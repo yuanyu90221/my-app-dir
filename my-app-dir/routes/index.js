e96f0b5c78e82171b023ae0136a2c064e037f872
yuanyu90221 <yuanyu90221@gmail.com>
2017-12-31 02:45:16 +0800

    add loginForm and registerForm and registerValidation flow

--- my-app-dir/routes/index.js
+++ my-app-dir/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
+
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
@@ -29,4 +31,12 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug))
 
 router.get('/tags', catchErrors(storeController.getStoreByTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoreByTag))
+
+router.get('/login', userController.loginForm)
+router.get('/register', userController.registerForm)
+
+// 1. Validate the registration data
+// 2. register the user
+// 3. we need to log them in
+router.post('/register', userController.validateRegister)
 module.exports = router;
