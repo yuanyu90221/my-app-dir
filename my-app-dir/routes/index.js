ca08108df37c0d7a8c8d82c7382077be29c4df8c
yuanyu90221 <yuanyu90221@gmail.com>
2018-01-03 05:07:15 +0800

    add reset password flow

--- my-app-dir/routes/index.js
+++ my-app-dir/routes/index.js
@@ -50,4 +50,10 @@ router.get('/logout', authController.logout)
 
 router.get('/account', authController.isLoggedIn, userController.account)
 router.post('/account', catchErrors(userController.updateAccount))
+router.post('/account/forgot', catchErrors(authController.forgot))
+router.get('/account/reset/:token', catchErrors(authController.reset))
+router.post('/account/reset/:token', 
+  authController.confirmedPasswords, 
+  catchErrors(authController.udpate)
+)
 module.exports = router;
