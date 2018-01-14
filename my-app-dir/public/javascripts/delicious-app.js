1b084b746bf3b585d749b2b8bea78f530ec8d2e6
yuanyu90221 <yuanyu90221@gmail.com>
2018-01-15 03:15:43 +0800

    add typeAhead flow

--- my-app-dir/public/javascripts/delicious-app.js
+++ my-app-dir/public/javascripts/delicious-app.js
@@ -2,6 +2,10 @@ import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
 
-import autocomplete from './modules/autocomplete'
+import autocomplete from './modules/autocomplete';
 
-autocomplete( $('#address'), $('#lat'), $('#lng'))
+import typeAhead from './modules/typeAhead';
+
+autocomplete( $('#address'), $('#lat'), $('#lng'));
+
+typeAhead($('.search'));
